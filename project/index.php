59194fb640db808bf9b2d771500289ddbcae30e2
breannahaycook <105466497+breannahaycook@users.noreply.github.com>
2022-11-06 17:57:28 -0500

    Verified all Model Functions Worked
    Checked all functions and added Delete and Update Reviews Functionality

--- project/index.php
+++ project/index.php
@@ -2,35 +2,64 @@
 try {
     require_once 'model/database_model.php';
     require_once 'model/user_model.php';
+    require_once 'model/login_model.php';
+    require_once 'model/breed_info_model.php';
+    require_once 'model/breed_review_model.php';
+    
+    $email_address = htmlspecialchars(filter_input(INPUT_POST, "email_address"));
+    $password = htmlspecialchars(filter_input(INPUT_POST, "password"));
     
     echo $database_message;
     echo "Testing";
 
-    // create a user !! Breanna - SUCCESSFUL !!
+    // create a user !!
 //    $password_hash = password_hash("test", PASSWORD_DEFAULT);
 //    $user = new User("Thumper", "thaycook@gmail.com", $password_hash);
-//    create_users($user);
-    
-    // create a user !! Rafi
-//    $password_hash = password_hash("test", PASSWORD_DEFAULT);
-//    $user = new User("rafiTest", "rafiTest@gmail.com", $password_hash);
-//    create_users($user);
-    
-    // create a user !! Kaitlyn
-//    $password_hash = password_hash("test", PASSWORD_DEFAULT);
-//    $user = new User("kaitlynTest", "kaitlynTest@gmail.com", $password_hash, 0);
 //    create_users($user);
     
     //  get all users
     $all_users = get_all_users();
     
-//    // get user by email address
-    $email_address = "bmhaycook@gmail.com";
-    $one_user_email = get_user_by_email($email_address);
+    // get user by email address
+    $get_user_email_address = "bmhaycook@gmail.com";
+    $one_user_email = get_user_by_email($get_user_email_address);
     
     // get user by id
-    $id = "6";
-    $one_user_id = get_user_by_id($id);
+    $get_user_id = "6";
+    $one_user_id = get_user_by_id($get_user_id);
+    
+    // login with email "bmhayook@gmail.com" and pass "test"
+    $loginTest = login($email_address, $password);
+    
+    // get all dog breeds
+    $all_breeds = get_all_breeds();
+    
+    // get dog breed by name
+    $breed_info_by_name = "Poodle";
+    $one_breed_by_name = get_breed_by_name($breed_info_by_name);
+    
+    // create review
+//    $review = new Review(2, "Poodle", "PoodleReview", "", 0);
+//    create_review($review);
+    
+    // get all breed reviews
+    $all_breed_reviews = get_all_breed_reviews();
+    
+    // get breed review by name
+    $breed_review_name = "Poodle";
+    $one_breed_review_by_name = get_breed_reviews_by_name($breed_review_name);
+    
+    // get breed review by user id
+    $breed_review_user_id = 1;
+    $breed_reviews_by_user_id = get_breed_reviews_by_user_id($breed_review_user_id);
+    
+    // delete review 
+//    $id = 6;
+//    delete_review($id);
+    
+    // update review
+//    $review = new Review(0, "", "PoodleReview2", "", 1);
+//    update_review($review);
 
     
 } catch (Exception $e) {
@@ -48,6 +77,7 @@ try {
     </head>
     <body>
         <h2>All Users Table</h2>
+        <h5><i>get_all_users()</i></h5>
         <table>
             <tr>
                 <th>Name</th>
@@ -63,7 +93,11 @@ try {
             <?php endforeach; ?>
         </table>
         
+        <hr>
+        
         <h2>User By Email Table</h2>
+        <h5><i>get_user_by_email()</i></h5>
+        <h4><?php echo "Email: " . $get_user_email_address; ?></h4>
         <table>
             <tr>
                 <th>Name</th>
@@ -75,7 +109,11 @@ try {
             </tr>
         </table>
         
+        <hr>
+        
         <h2>User By ID Table</h2>
+        <h5><i>get_user_by_id()</i></h5>
+        <h4><?php echo "Id: " . $get_user_id; ?></h4>
         <table>
             <tr>
                 <th>Name</th>
@@ -85,6 +123,156 @@ try {
                 <td><?php echo $one_user_id[0]; ?></td>
                 <td><?php echo $one_user_id[1]; ?></td>
             </tr>
+        </table>
+        
+        <hr>
+        
+        <h2>Login Form</h2>
+        <h5><i>login()</i></h5>
+        <form action="index.php" method="post">
+            <label><h4>Email Address: </h4></label>
+            <input type="text" name="email_address"><br>
+            <label><h4>Password: </h4></label>
+            <input type="password" name="password"><br>
+            <label>&nbsp;</label><br>
+            <input type="submit" value="login"><br>
+        </form>
+        <?php echo "Login Test Result: " . $loginTest . "<br><br> Note: <br> - Success: \"1\"<br> - Failed: \" \""?>
+
+        <hr>
+        
+        <h2>All Dog Breeds Table</h2>
+        <h5><i>get_all_breeds()</i></h5>
+        <table>
+            <tr>
+                <th>Breed</th>
+                <th>Size</th>
+                <th>Training Level</th>
+                <th>Hypoallergenic</th>
+                <th>Activity Level</th>
+                <th>Noise Level</th>
+                <th>Kids Temp</th>
+                <th>Grooming Level</th>
+                <th>Household Size</th>
+                <th>Shedding</th>
+                <th>Information</th>
+                <th>Id</th>
+            </tr>
+            <?php foreach($all_breeds as $breed_info) : ?>
+            <tr>
+                <td><?php echo $breed_info-> get_breed(); ?></td>
+                <td><?php echo $breed_info-> get_size(); ?></td>
+                <td><?php echo $breed_info-> get_training_level(); ?></td>
+                <td><?php echo $breed_info-> get_hypoallergenic(); ?></td>
+                <td><?php echo $breed_info-> get_activity_level(); ?></td>
+                <td><?php echo $breed_info-> get_noise_level(); ?></td>
+                <td><?php echo $breed_info-> get_kids_temp(); ?></td>   
+                <td><?php echo $breed_info-> get_grooming(); ?></td>
+                <td><?php echo $breed_info-> get_household_size(); ?></td> 
+                <td><?php echo $breed_info-> get_shedding(); ?></td>
+                <td><?php echo $breed_info-> get_information(); ?></td>
+                <td><?php echo $breed_info-> get_id(); ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+        
+        <hr>
+        
+        <h2>Dog Breed By Name Table</h2>
+        <h5><i>get_breed_by_name()</i></h5>
+        <h4><?php echo "Breed: " . $breed_info_by_name; ?></h4>
+        <table>
+            <tr>
+                <th>Size</th>
+                <th>Training Level</th>
+                <th>Hypoallergenic</th>
+                <th>Activity Level</th>
+                <th>Noise Level</th>
+                <th>Kids Temp</th>
+                <th>Grooming Level</th>
+                <th>Household Size</th>
+                <th>Shedding</th>
+                <th>Information</th>
+                <th>Id</th>
+            </tr>
+            <tr>
+                <td><?php echo $one_breed_by_name[0] ?></td>
+                <td><?php echo $one_breed_by_name[1] ?></td>
+                <td><?php echo $one_breed_by_name[2] ?></td>
+                <td><?php echo $one_breed_by_name[3] ?></td>
+                <td><?php echo $one_breed_by_name[4] ?></td>
+                <td><?php echo $one_breed_by_name[5] ?></td>
+                <td><?php echo $one_breed_by_name[6] ?></td>
+                <td><?php echo $one_breed_by_name[7] ?></td>
+                <td><?php echo $one_breed_by_name[8] ?></td>
+                <td><?php echo $one_breed_by_name[9] ?></td>
+                <td><?php echo $one_breed_by_name[10] ?></td>                                                                    
+            </tr>
+        </table>
+        
+        <hr>
+        
+        <h2>All Breed Reviews Table</h2>
+        <h5><i>get_all_breed_reviews()</i></h5>
+        <table>
+            <tr>
+                <th>User Id</th>
+                <th>Breed</th>
+                <th>Review</th>
+                <th>Timestamp</th>
+                <th>Id</th>
+            </tr>
+            <?php foreach($all_breed_reviews as $breed_reviews) : ?>
+            <tr>
+                <td><?php echo $breed_reviews-> get_user_id(); ?></td>
+                <td><?php echo $breed_reviews-> get_breed(); ?></td>
+                <td><?php echo $breed_reviews-> get_review(); ?></td>
+                <td><?php echo $breed_reviews-> get_timestamp(); ?></td>
+                <td><?php echo $breed_reviews-> get_id(); ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+        
+        <hr>
+        
+        <h2>Dog Review By Breed Name Table</h2>
+        <h5><i>get_breed_reviews_by_name($breed)</i></h5>
+        <h4><?php echo "Breed: " . $breed_review_name; ?></h4>
+        <table>
+            <tr>
+                <th>User Id</th>
+                <th>Review</th>
+                <th>Timestamp</th>
+                <th>Id</th>
+            </tr>
+            <tr>
+                <td><?php echo $one_breed_review_by_name[0] ?></td>
+                <td><?php echo $one_breed_review_by_name[1] ?></td>
+                <td><?php echo $one_breed_review_by_name[2] ?></td>
+                <td><?php echo $one_breed_review_by_name[3] ?></td>                                                                
+            </tr>
+        </table>
+        
+        <hr>
+        
+        <h2>Dog Breed By User Id</h2>
+        <h5><i>get_breed_reviews_by_user_id($user_id)</i></h5>
+        <h4><?php echo "User_Id: " . $breed_review_user_id; ?></h4>
+        <table>
+            <tr>
+                <th>Breed</th>
+                <th>Review</th>
+                <th>Timestamp</th>
+                <th>Id</th>
+            </tr>
+            <?php foreach($breed_reviews_by_user_id as $user_breed_reviews) : ?>
+            <tr>
+                <td><?php echo $user_breed_reviews-> get_breed(); ?></td>
+                <td><?php echo $user_breed_reviews-> get_review(); ?></td>
+                <td><?php echo $user_breed_reviews-> get_timestamp(); ?></td>
+                <td><?php echo $user_breed_reviews-> get_id(); ?></td>                                                               
+            </tr>
+            <?php endforeach; ?>
         </table>
     </body>
 </html>
--- project/model/breed_review_model.php
+++ project/model/breed_review_model.php
@@ -1,63 +1,54 @@
 <?php
 
 class Review {
-    private $user_id, $name, $breed_name, $review, $timestamp, $id;
+    private $user_id, $breed, $review, $timestamp, $id;
     
-    public function __construct($user_id, $name, $breed_name, $review, $timestamp, $id = 0){
+    public function __construct($user_id, $breed, $review, $timestamp, $id = 0){
         $this-> set_user_id($user_id);
-        $this-> set_name($name);
-        $this-> set_breed_name($breed_name);
+        $this-> set_breed($breed);
         $this-> set_review($review);
         $this-> timestamp = $timestamp;
         $this-> id = $id;
     }
     
     public function get_user_id() {
-        return $this->user_id;
+        return $this-> user_id;
     }
 
-    public function get_name() {
-        return $this->name;
-    }
-
-    public function get_breed_name() {
-        return $this->breed_name;
+    public function get_breed() {
+        return $this-> breed;
     }
 
     public function get_review() {
-        return $this->review;
+        return $this-> review;
     }
 
     public function get_timestamp() {
-        return $this->timestamp;
+        return $this-> timestamp;
     }
 
     public function get_id() {
-        return $this->id;
+        return $this-> id;
     }
 
     public function set_user_id($user_id) {
-        $this->user_id = $user_id;
+        $this-> user_id = $user_id;
     }
 
-    public function set_name($name) {
-        $this->name = $name;
-    }
-
-    public function set_breed_name($breed_name) {
-        $this->breed_name = $breed_name;
+    public function set_breed($breed) {
+        $this-> breed = $breed;
     }
 
     public function set_review($review) {
-        $this->review = $review;
+        $this-> review = $review;
     }
 }
 
-function get_all_reviews() {
+function get_all_breed_reviews() {
     global $database;
     
     // reads breed review info
-    $reviews_query = 'SELECT user_id, name, breed_name, review, timestamp, id FROM breed_experience';
+    $reviews_query = 'SELECT user_id, breed, review, timestamp, id FROM breed_review';
     $reviews_statement = $database->prepare($reviews_query);
     $reviews_statement->execute();
     $reviews = $reviews_statement->fetchAll();
@@ -66,8 +57,41 @@ function get_all_reviews() {
     $reviews_array = array();
     
     foreach($reviews as $review) {
-        $reviews_array[] = new User($review['user_id'], $review['name'],
-            $review['breed_name'], $review['review'], $review['timestamp'], $review['id']);
+        $reviews_array[] = new Review($review['user_id'], $review['breed'], $review['review'], $review['timestamp'], $review['id']);
+    }
+    
+    return $reviews_array;
+}
+
+function get_breed_reviews_by_name($breed) {
+    global $database;
+    
+    // reads breed review info by breed name
+    $reviews_query = 'SELECT user_id, review, timestamp, id FROM breed_review WHERE breed = :breed';
+    $reviews_statement = $database->prepare($reviews_query);
+    $reviews_statement->bindValue(":breed", $breed);
+    $reviews_statement->execute();
+    $reviews = $reviews_statement->fetch();
+    $reviews_statement->closeCursor();
+    
+    return $reviews;
+}
+
+function get_breed_reviews_by_user_id($user_id) {
+    global $database;
+    
+    // reads breed review info by user id
+    $reviews_query = 'SELECT user_id, breed, review, timestamp, id FROM breed_review WHERE user_id = :user_id';
+    $reviews_statement = $database->prepare($reviews_query);
+    $reviews_statement->bindValue(":user_id", $user_id);
+    $reviews_statement->execute();
+    $reviews = $reviews_statement->fetchAll();
+    $reviews_statement->closeCursor();
+    
+    $reviews_array = array();
+    
+    foreach($reviews as $review) {
+        $reviews_array[] = new Review($review['user_id'], $review['breed'], $review['review'], $review['timestamp'], $review['id']);
     }
     
     return $reviews_array;
@@ -77,15 +101,37 @@ function create_review($review) {
     global $database;
 
     // create new breed review
-    $reviews_query = "INSERT INTO breed_experience (user_id, name, breed_name, review) VALUES (:user_id, :name, :breed_name, :review)";
+    $reviews_query = "INSERT INTO breed_review (user_id, breed, review) VALUES (:user_id, :breed, :review)";
 
     $reviews_statement = $database->prepare($reviews_query);
     $reviews_statement->bindValue(":user_id", $review-> get_user_id());
-    $reviews_statement->bindValue(":name", $review-> get_name());
-    $reviews_statement->bindValue(":breed_name", $review-> get_breed_name());
+    $reviews_statement->bindValue(":breed", $review-> get_breed());
     $reviews_statement->bindValue(":review", $review-> get_review());
     $reviews_statement->execute();
     $reviews_statement->closeCursor();
 }
 
-// Add Delete(Preferred) or Edit review
+function update_review($review) {
+    global $database;
+
+    // update breed review
+    $tweets_query = "UPDATE breed_review SET review = :review WHERE id = :id";
+
+    $reviews_statement = $database->prepare($tweets_query);
+    $reviews_statement->bindValue(":id", $review-> get_id());
+    $reviews_statement->bindValue(":review", $review-> get_review());
+    $reviews_statement->execute();
+    $reviews_statement->closeCursor();
+}
+
+function delete_review($id) {
+    global $database;
+
+     // delete breed review
+    $reviews_query = "DELETE FROM breed_review WHERE id = :id";
+
+    $reviews_statement = $database->prepare($reviews_query);
+    $reviews_statement->bindValue("id", $id);
+    $reviews_statement->execute();
+    $reviews_statement->closeCursor();
+}
