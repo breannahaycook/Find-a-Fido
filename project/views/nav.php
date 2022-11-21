b3bbe68a7d79c13d81273746a2e5268a0406bc6b
hassanrafiul <rafiulhassan@outlook.com>
2022-11-21 17:57:32 -0500

    nov1
    updated

---
+++ project/views/nav.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="style.css" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/804adbe216.js" crossorigin="anonymous"></script>
+        <title>Document</title>
+        <style>
+
+            @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
+
+            :root {
+                --lightblue: #B9DEE7;
+                --navbar:#77CCFF;
+                --button: #A91B0D;
+                --buttonafter: #60100B;
+                --hovercolor: #253DA1;
+            }
+
+
+            *{
+                padding: 0;
+                margin: 0;
+                box-sizing: border-box;
+                font-family: 'Roboto', sans-serif;
+            }
+
+
+            .header{
+                width: 100%;
+                height: 120px;
+                background-color: var(--lightblue);
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                border-radius: 20px;
+                padding: 0 100px;
+
+            }
+            .nav__logo{
+                height: 105px;
+                border-radius: 25px;
+
+                margin-top: 5px;
+                margin-bottom: 5px;
+                margin-right: 11px;
+            }
+
+            .hamburger{
+                display: none;
+            }
+            .head{
+                font-size: 30px;
+
+            }
+            .head_f{
+                font-size: 40px;
+                color: blue;
+            }
+
+            .nav__label{
+                display: flex;
+                justify-content: space-around;
+                align-items: center;
+            }
+
+            .nav__items{
+                display: flex;
+                margin-right: 20px;
+
+            }
+
+            .nav__lists{
+                list-style: none;
+                text-transform: uppercase;
+                padding-right: 50px;
+
+            }
+
+
+            .nav__tags{
+                text-decoration: none;
+                color: white;
+                padding: 8px 10px;
+            }
+
+            .nav__tags:hover{
+                background-color: var(--hovercolor);
+                transition: all 0.5s ease-in-out;
+                border-radius: 100px;
+                color: black;
+
+            }
+            .btn{
+                background-color: var(--button);
+                transition: all 0.5s ease-in-out;
+                border-radius: 100px;
+                color: black;
+            }
+
+            .checkbtn{
+                color: var(--hovercolor);
+                font-size: 25px;
+                cursor: pointer;
+                float: right;
+                display: none;
+            }
+
+            #check{
+                display: none;
+            }
+
+
+            @media screen and (max-width: 1442px) {
+
+                .nav__lists{
+                    padding-right: 15px;
+                }
+
+
+            }
+            @media screen and (max-width: 1300px) {
+
+                .nav__lists{
+                    padding-right: 5px;
+                }
+
+
+            }
+
+            @media screen and (max-width: 1300px) {
+
+                .nav__lists{
+                    padding-right: 5px;
+                }
+                .header{
+                    padding: 20px;
+                }
+
+
+            }
+
+            @media screen and (max-width: 994px) {
+
+                .nav__lists{
+                    padding-right: 2px;
+                }
+                .header{
+                    padding: 5px;
+                }
+
+
+            }
+
+            @media screen and (max-width: 990px) {
+
+                .header{
+                    display: flex;
+                    justify-content: space-around;
+                    align-items: center;
+                }
+                .checkbtn{
+                    display: block;
+                    height: 20px;
+                    padding-right: 50px;
+
+                }
+                .nav__logo{
+                    height: 80px;
+                    border-radius: 25px;
+                    margin-top: 5px;
+                    margin-bottom: 5px;
+                    margin-right: 11px;
+                }
+                .nav__items{
+                    display: none;
+                }
+
+                /* .nav__items{
+                    width: 100%;
+                    height: 100vh;
+                    background-color: var(--lightblue);
+                    
+                    top: 80px;
+                }
+                .nav__lists{
+                    display: block;
+                    margin: 60px 0px;
+                } */
+
+
+
+            }
+            @media screen and (max-width: 410px){
+
+                .checkbtn{
+                    padding-left: 10px;
+
+                }
+                .head{
+                    font-size: 22px;
+
+                }
+
+                .head_f{
+                    font-size: 22px;
+                    color: blue;
+                }
+                .nav__logo{
+                    height: 80px;
+                    border-radius: 25px;
+                    margin-top: 5px;
+                    margin-bottom: 5px;
+                    margin-right: 11px;
+                }
+
+            }
+
+
+
+
+
+
+
+        </style>
+    </head>
+    <body>
+        <header class="header">
+            <div class="nav__label">
+                <img class="nav__logo" src="../utility/assets/Picture1.jpg" alt="Company Logo!">
+                <h1 class="head">Find-A-<span class="head_f">Fido!</span></h1>  
+            </div>
+            <nav class="nav__bar">    
+                <ul class="nav__items">
+                    <li class="nav__lists"><a class="nav__tags active" href="home.html">Home</a></li>
+                    <li class="nav__lists"><a class="nav__tags" href="DogBreed.html">Dog Breed</a></li>
+                    <li class="nav__lists"><a class="nav__tags" href="DogCare.html">Dog Care</a></li>
+                    <li class="nav__lists"><a class="nav__tags" href="Reviews.html">Reviews</a></li>
+                    <li class="nav__lists"><a class="nav__tags" href="AboutUs.html">About Us</a></li>
+                    <li class="nav__lists"><a class="nav__tags btn" href="LogIn.html">Log In / Sign UP</a></li>
+                </ul>
+            </nav>
+            <input type="checkbox" id="check">
+            <label for="check" class="checkbtn">
+
+                <i class="fa-solid fa-bars"></i>
+
+            </label>
+
+        </header>        
+    </body>
+
+</html>
